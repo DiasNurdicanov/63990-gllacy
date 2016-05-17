b4bb04165796b86b05e7b52104998f0e45902acb
DiasNurdicanov <dison08@mail.ru>
2016-05-17 22:54:32 +0600

    Правки

--- js/script.js
+++ js/script.js
@@ -20,3 +20,24 @@ close.addEventListener('click', function(event) {
 
 	form.classList.remove('show');
 });
+
+//Карта
+ymaps.ready(init);
+var myMap, 
+    myPlacemark;
+
+function init(){ 
+    myMap = new ymaps.Map("map", {
+        center: [59.938631, 30.323055],
+        zoom: 16
+    }); 
+    
+    myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/map-marker.svg',
+        iconImageSize: [218, 142],
+        iconImageOffset: [-50, -142]
+    });
+    
+    myMap.geoObjects.add(myPlacemark);
+}
